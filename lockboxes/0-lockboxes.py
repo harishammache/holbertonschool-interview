c382440aede20f7590260d38db142f383035c644
harishammache <hammache_haris@hotmail.fr>
2025-04-16 15:13:12 +0200

    feat: implement canUnlockAll function for lockboxes problem

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''
+    Lockboxes
+'''
+
+
+def canUnlockAll(boxes):
+    """method that determines if all the boxes can be opened"""
+    n = len(boxes)
+    boites_ouvertes = {0}
+
+    cles_a_explorer = boxes[0].copy()
+
+    while len(cles_a_explorer) > 0:
+        cle = cles_a_explorer.pop()
+
+        if cle < n and cle not in boites_ouvertes:
+            boites_ouvertes.add(cle)
+        for nouvelle_cle in boxes[cle]:
+            if nouvelle_cle not in boites_ouvertes:
+                cles_a_explorer.append(nouvelle_cle)
+
+    if len(boites_ouvertes) == n:
+        return True
+    else:
+        return False

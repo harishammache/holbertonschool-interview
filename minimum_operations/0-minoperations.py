ec5c015b9556ba96818c4a15641f67ef9f701080
harishammache <hammache_haris@hotmail.fr>
2025-05-22 10:42:44 +0200

    feat: implement minOperations using factorization strategy

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    write a method that calculates the fewest number of operations
+    needed to result in exactly n H characters in the file
+"""
+
+
+def minOperations(n):
+    """
+
+        Returns an integer
+        If n is impossible to achieve, return 0
+    """
+    if n < 2:
+        return 0
+    
+    ops = 0
+    facteur = 2
+
+    while n > 1:
+        while n % facteur == 0:
+            ops += facteur
+            n = n // facteur
+        facteur += 1
+
+    return ops

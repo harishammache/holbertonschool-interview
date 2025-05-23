933eab751a14d9442a8023eb9481bb93aea79342
harishammache <hammache_haris@hotmail.fr>
2025-05-23 12:09:26 +0200

    feat: implement plaindrome check for singly linked list

---
+++ linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * function in C that checks if a singly linked list is a palindrome
+ * @listint_t **head: attribut
+ * 
+ * Return 0 if it not a palindrome, 1 if it is a palindrome
+ */
+int is_palindrome(listint_t **head){
+    listint_t *slow = *head;
+    listint_t *fast = *head;
+    listint_t *next, *prev, *second_half;
+    if (*head == NULL || (*head)->next == NULL){
+        return (1);
+    }
+    
+    while (fast != NULL && fast->next != NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    while (slow != NULL){
+        next = slow->next;
+        slow->next = prev;
+        prev = slow;
+        slow = next;
+    }
+
+    second_half = prev;
+
+    while (second_half != NULL){
+        if(slow->n != second_half->n){
+            return 1; 
+        }
+        slow = slow->next;
+        second_half = second_half->next;
+    }
+
+    return 0;
+}

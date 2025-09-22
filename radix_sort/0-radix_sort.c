bdf81f2687edb7d216a727a4cdae6d5e4ff1abdc
harishammache <hammache_haris@hotmail.fr>
2025-09-22 14:52:52 +0200

    fix(radix_sort): remove unused static function declaration from header

---
+++ radix_sort/0-radix_sort.c
@@ -0,0 +1,61 @@
+#include "sort.h"
+/**
+ * get_max - function that sorts an array of integers in ascending order
+ *
+ * @array: contain only numbers >= 0
+ * @size: size of the array
+ *
+ * Return: the value max on the array
+ */
+static int get_max(int *array, size_t size)
+{
+	size_t index;
+	int max = array[0];
+
+	for (index = 0; index < size; index++)
+	{
+		if (array[index] > max)
+			max = array[index];
+	}
+	return (max);
+}
+
+static void counting_sort(int *array, size_t size, int exp)
+{
+    int *output = malloc(sizeof(int) * size);
+    int count[10] = {0};
+
+    if (!output)
+        return;
+
+    for (size_t i = 0; i < size; i++)
+        count[(array[i] / exp) % 10]++;
+
+    for (int i = 1; i < 10; i++)
+        count[i] += count[i - 1];
+
+    for (ssize_t i = size - 1; i >= 0; i--)
+    {
+        output[count[(array[i] / exp) % 10] - 1] = array[i];
+        count[(array[i] / exp) % 10]--;
+    }
+
+    for (size_t i = 0; i < size; i++)
+        array[i] = output[i];
+
+    print_array(array, size);
+    free(output);
+}
+
+/* Tri radix LSD */
+void radix_sort(int *array, size_t size)
+{
+    if (!array || size < 2)
+        return;
+
+    int max = get_max(array, size);
+
+    for (int exp = 1; max / exp > 0; exp *= 10)
+        counting_sort(array, size, exp);
+}
+
---
+++ radix_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include <sys/types.h>
+void radix_sort(int *array, size_t size);
+void print_array(const int *array, size_t size);
+
+#endif 

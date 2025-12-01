c332d0157fe22affb937bda321f2b85221ff44ba
harishammache <hammache_haris@hotmail.fr>
2025-12-01 17:16:58 +0100

    refactor(is_palindrome): simplify palindrome check by removing unnecessary digit extraction

--- palindrome_integer/0-is_palindrome.c
+++ palindrome_integer/0-is_palindrome.c
@@ -1,49 +1,20 @@
 #include "palindrome.h"
 
 /**
- * is_palindrome - checks whether a given unsigned integer is a palindrome
- * @n: the number to be checked
+ * is_palindrome - checks if an unsigned long integer is a palindrome
+ * @n: number to check
  *
- * Return: 1 if n is a palindrome, and 0 otherwise
+ * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(unsigned long n)
 {
-    unsigned long temp = n;
-    char digits[21]; /* Max digits for 64-bit unsigned long is 20 */
-    int len = 0;
-    int i;
+    unsigned long reversed = 0, tmp = n;
 
-    /* Extract digits into array */
-    do {
-        digits[len++] = temp % 10;
-        temp /= 10;
-    } while (temp != 0);
-
-}
-
-/**
- * is_palindrome - checks whether a given unsigned integer is a palindrome
- * @n: the number to be checked
- *
- * Return: 1 if n is a palindrome, and 0 otherwise
- */
-int is_palindrome(unsigned long n)
-{
-    unsigned long temp = n;
-    char digits[21];
-    int len = 0;
-    int i;
-
-    do {
-        digits[len++] = temp % 10;
-        temp /= 10;
-    } while (temp != 0);
-
-    for (i = 0; i < len / 2; i++)
+    while (tmp != 0)
     {
-        if (digits[i] != digits[len - 1 - i])
-            return (0);
+        reversed = reversed * 10 + (tmp % 10);
+        tmp /= 10;
     }
 
-    return (1);
+    return (reversed == n ? 1 : 0);
 }
